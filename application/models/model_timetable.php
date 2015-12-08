0602aa79346c0d9f6b7bfabce77d6af036df276f
Mark <mrak.mk@ya.ru>
2015-12-08 16:32:27 +1000

    При добавлении события можно выбрать группу
    В таблицу event добавляется ID выбранной группы

--- application/models/model_timetable.php
+++ application/models/model_timetable.php
@@ -18,16 +18,24 @@ class Model_Timetable extends Model
         $query->bindParam(':id', $id);
         $query->execute();
     }
-    public function addEvent($name, $dateYMD, $description, $task)
+    public function addEvent($name, $dateYMD, $description, $task, $groupID)
     {
-        $query = $this->DBH->prepare("INSERT INTO event SET name=:name,date=:dateYMD,description=:description,task=:task");
+        $query = $this->DBH->prepare("INSERT INTO event SET name=:name,date=:dateYMD,description=:description,task=:task, group_id=:groupID");
         $query->bindParam(':name', $name);
         $query->bindParam(':dateYMD', $dateYMD);
         $query->bindParam(':description', $description);
         $query->bindParam(':task', $task);
+        $query->bindParam(':groupID', $groupID);
         $query->execute();
         $lastid = $this->DBH->lastInsertId();
         $this->addMark($lastid);
+    }
+    public function getCategory()
+    {
+        $query = $this->DBH->prepare("SELECT id, name FROM category");
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
     }
 }
 ?>
