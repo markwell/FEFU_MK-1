36633ef9e9f8b7602cf46b088b301155638fb718
Mark <mrak.mk@ya.ru>
2015-12-08 20:06:46 +1000

    Дизайн календаря

--- application/views/timetable_view.php
+++ application/views/timetable_view.php
@@ -1,4 +1,5 @@
 <?php global $HTTP_POST_VARS; ?>
+  <h2>Расписание</h2><br>
 <div class="form_container">
         <form id="event_form" method="POST" action="/shop/timetable/addevent">
         <h3>Добавление события</h3>
@@ -7,16 +8,15 @@
                 <input type="date" name="dateFF" class="form-control" id="date" placeholder="YYYY-MM-DD">
             </div>
             <div class="form-group">
-                <textarea id="description" name="descriptionFF" placeholder="Описание события"></textarea>
+                <textarea id="description" name="descriptionFF" class="form-control" placeholder="Описание события"></textarea>
             </div>
             <div class="form-group">
-                <textarea id="task" name="taskFF" placeholder="Описание задания к событию (если есть)"></textarea>
+                <textarea id="task" name="taskFF" class="form-control" placeholder="Описание задания к событию (если есть)"></textarea>
             </div>
             <div class="form-group text-center">
               <label for="exampleInputPassword2">Номер группы</label><br/>
               <div class="btn-group">
               <p><select size="1" name="groupFF" class="btn  dropdown-toggle">
-                  <option value="0" selected>Не знаю в какой я группе</option>
                   <?php
                     for ($i=1; $i <= count($HTTP_POST_VARS); $i++) { 
                       if (isset($HTTP_POST_VARS[$i]['name']))  {
@@ -46,12 +46,13 @@
         $d = 1;
         $i = 1;
         $currentdate = date('Y-m-d');
-        while ($i<20)
+        $date = date('jS MS');
+        while ($i<19)
         { 
            echo ' 
-           <div class="col-md-1 calendar_day">
+           <div class="col-md-3 calendar_day">
                <div class="date">
-                   '.$currentdate.'
+                   <p class="lead">'.$date.'</p>
                </div>
                <div class="scroll_image"></div>
                <div class="list_events">
@@ -66,6 +67,7 @@
                </div>
            </div>";
            $currentdate = date ('Y-m-d', strtotime ('+'.$i.' days'));
+           $date = date('jS MS', strtotime ('+'.$i.' days'));
            $d++;
            $i++;
         }
